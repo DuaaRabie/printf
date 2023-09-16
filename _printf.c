f2926764f80cc6215142e31990c350c746f44e81
DuaaRabie <Duaarabie11@gmail.com>
2023-09-16 07:56:20 -0700

    printf with width

--- _printf.c
+++ _printf.c
@@ -1,9 +1,33 @@
 #include "main.h"
 
 /**
- * print_int - print integer
+ * print_space : print space for width
+ * @num: the number
+ * @w: the width
+ * Return: nothing
+ */
+void print_space(long int num, int w)
+{
+	int count = 1;
+
+	if (num < 0)
+		num *= -1;
+	while (num / 10)
+	{
+		count++;
+		num /= 10;
+	}
+	while (w - count > 0)
+	{
+		write(1, " ", 1);
+		w--;
+	}
+}
+
+/**
+ * pritn_int - print integers
  * @c: integer to print
- * Return: 0
+ * Return: nothing
  */
 void print_int(int c)
 {
@@ -19,9 +43,28 @@ void print_int(int c)
 	digit = c % 10 + 48;
 	write(1, &digit, 1);
 }
-
 /**
- * _printf : prints a format
+ * check_width - check width and print needed spaces
+ * @num: the integer
+ * @format: the format of printing
+ * @i: the index
+ * Return: nothing
+ */
+int check_width(int num, const char *format, int i)
+{
+	if (format[i + 2] == 'd' || format[i + 2] == 'i')
+	{
+		if (format[i + 1] > '0' && format[i + 1] <= '9')
+		{
+			print_space(num, format[i + 1] - 48);
+			print_int(num);
+			i +=2;
+		}
+	}
+	return (i);
+}
+/**
+ * _printf - prints a format
  * @format: the format to be printed
  * ...: variadic arguments
  * Return: number of printed characters
@@ -34,41 +77,53 @@ int _printf(const char *format, ...)
 	long int num;
 	int c, i = 0;
 
-
 	va_start(args, format);
 	while (format && format[i])
 	{
 		if (format[i] == '%')
 		{
-			i++;
-			switch(format[i])
+		/*	if (format[i + 2] == 'd' || format[i + 2] == 'i')
 			{
-				case '%':
-					c = va_arg(args, int);
-					write(1, &c, 1);
-					break;
-				case 'c':
-					c = va_arg(args, int);
-					write(1, &c, 1);
-					count++;
-					break;
-				case 's':
-					s = va_arg(args, char*);
-					j = 0;
-					while (s[j])
-					{
-						write(1, s + j, 1);
-						count++;
-						j++;
-					}
-					break;
-				case 'i':
-				case 'd':
+				if (format[i + 1] > '0' && format[i + 1] <= '9')
+				{
 					num = va_arg(args, long int);
+					print_space(num, format[i + 1] - 48);
 					print_int(num);
-					break;
-				default:
-					continue;
+					i += 2;
+				}
+			}*/
+	
+			{
+				i++;
+				switch(format[i])
+				{
+					case '%':
+						c = va_arg(args, int);
+						write(1, &c, 1);
+						break;
+					case 'c':
+						c = va_arg(args, int);
+						write(1, &c, 1);
+						count++;
+						break;
+					case 's':
+						s = va_arg(args, char*);
+						j = 0;
+						for (j = 0; s[j] != '\0'; j++)
+						{
+							write(1, s + j, 1);
+							count++;
+						}
+						break;
+					case 'i':
+					case 'd':
+						num = va_arg(args, long int);
+						i = check_width(num, format, i);
+						print_int(num);
+						break;
+					default:
+						continue;
+				}
 			}
 		}
 		else
@@ -85,7 +140,7 @@ int _printf(const char *format, ...)
 
 int main(void)
 {
-	_printf("myc name: %s, %c, %i yes \n", "Duaa", 'D', -2035065302);
-	_printf("myc name: %s, %c, %d no \n", "Omar", 'O', 1560133635);
+	_printf("myc name: %s, %c, %8d yes\n", "Duaa", 'D', 12);
+	_printf("myc name: %s, %c, %i no\n", "Omar", 'O', -2035065302);
 	return 0;
 }
