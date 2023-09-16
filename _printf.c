a4dbfa6fd2cc1d3815e73f7ff4aabbf278027592
DuaaRabie <Duaarabie11@gmail.com>
2023-09-15 17:15:44 -0700

    handle basic d and i conversion specifiers

--- _printf.c
+++ _printf.c
@@ -1,5 +1,24 @@
 #include "main.h"
-#include <string.h>
+
+/**
+ * print_int - print integer
+ * @c: integer to print
+ * Return: 0
+ */
+void print_int(int c)
+{
+	char digit;
+
+	if (c < 0)
+	{
+		write(1, "-", 1);
+		c *= -1;
+	}
+	if (c / 10 != 0)
+		print_int(c / 10);
+	digit = c % 10 + 48;
+	write(1, &digit, 1);
+}
 
 /**
  * _printf : prints a format
@@ -12,9 +31,8 @@ int _printf(const char *format, ...)
 	va_list args;
 	unsigned j, count = 0;
 	char *s;
+	long int num;
 	int c, i = 0;
-	char buffer[20];
-	(void) buffer;
 
 
 	va_start(args, format);
@@ -46,6 +64,8 @@ int _printf(const char *format, ...)
 					break;
 				case 'i':
 				case 'd':
+					num = va_arg(args, long int);
+					print_int(num);
 					break;
 				default:
 					continue;
@@ -65,7 +85,7 @@ int _printf(const char *format, ...)
 
 int main(void)
 {
-	_printf("myc name: %s, %c\n", "Duaa", 'D');
-	_printf("myc name: %s, %c\n", "Omar", 'O');
+	_printf("myc name: %s, %c, %i yes \n", "Duaa", 'D', -2035065302);
+	_printf("myc name: %s, %c, %d no \n", "Omar", 'O', 1560133635);
 	return 0;
 }
