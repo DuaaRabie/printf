d55e475b6389d40f9161e7fd7648e03f14e34d86
DuaaRabie <Duaarabie11@gmail.com>
2023-09-16 11:35:03 -0700

    to test the checker

---
+++ test/duaa_test.c
@@ -0,0 +1,59 @@
+#include "main.h"
+
+int main(void)
+{
+
+	_printf("myc name: %s, %c, %i\n", "Duaa", 'D', -15);
+	_printf("myc name: %s, %c, %d\n", "Duaa", 'D', -2035065302);
+/*	
+	_printf("myc name: %s, %c, %4d no\n", "Omar", 'O', -2035065302);
+	printf("myc name: %s, %c, %4d no\n", "Omar", 'O', -2035065302);
+
+	int len;
+	_printf("myc name: %s, %c, %i yes \n", "Duaa", 'D', -2035065302);
+	_printf("myc name: %s, %c, %d no \n", "Omar", 'O', 1560133635);
+	len = _printf("%b\n", 7);
+	_printf("%d\n", len);
+	printf("%S", "hi\n")
+	len = printf("%b\n", 7);
+	printf("%d\n", len);
+	len = _printf("%d\n", 214);
+	_printf("%d\n", len);
+
+
+  *
+  *
+  * int len;
+  * int len2;
+  * unsigned int ui;
+  * void *addr;
+  * len = _printf("Let's try to printf a simple sentence.\n");
+  * len2 = printf("Let's try to printf a simple sentence.\n");
+  * addr = (void *)0x7ffe637541f0;
+  * ui = (unsigned int)INT_MAX + 1024;
+  * _printf("%d\n", len);
+   *_printf("Length:[%d, %i]\n", len, len);
+   * printf("Length:[%d, %i]\n", len2, len2);
+   * _printf("Negative:[%d]\n", -762534);
+   * printf("Negative:[%d]\n", -762534);
+   * _printf("Unsigned:[%u]\n", ui);
+   * printf("Unsigned:[%u]\n", ui);
+   * _printf("Unsigned octal:[%o]\n", ui);
+   * printf("Unsigned octal:[%o]\n", ui);
+   * _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+   * printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+   * _printf("Character:[%c]\n", 'H');
+   * printf("Character:[%c]\n", 'H');
+   * _printf("String:[%s]\n", "I am a string !");
+   * printf("String:[%s]\n", "I am a string !");
+   * _printf("Address:[%p]\n", addr);
+   * printf("Address:[%p]\n", addr);
+   * len = _printf("Percent:[%%]\n");
+   * len2 = printf("Percent:[%%]\n");
+   * _printf("Len:[%d]\n", len);
+   * printf("Len:[%d]\n", len2);
+   * _printf("Unknown:[%r]\n");
+   * printf("Unknown:[%r]\n");
+   **/
+	return (0);
+}
---
+++ test/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+
+int _printf(const char *format, ...);
+
+/* handle hec */
+int to_hex(int n);
+int print_hex(const void *ptr);
+
+/* handle int with width and space */
+int print_space(long int num, int width);
+int print_int(int c);
+int check_width(int num, const char *format, int i);
+
+#endif
