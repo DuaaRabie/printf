cbd3b817b9a28ea14af060829594e043c5be85b1
oovaa <omarvx211@gmail.com>
2023-09-17 21:44:26 +0200

    edited a for loop at 17-21

--- _printf.c
+++ _printf.c
@@ -18,7 +18,7 @@ int _printf(const char *format, ...)
 	}
 
 	va_start(args, format);
-	for (i = 0; format[i] != '\0'; i++)
+	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] == '%' && format[i + 1] != '\0' && format[i + 1] != '%')
 		{
