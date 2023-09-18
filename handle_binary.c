2f4fad6d04941af47b3c22750bd5f9ecac37e953
oovaa <omarvx211@gmail.com>
2023-09-18 15:55:46 +0200

    update print_bin at 18-15

--- handle_binary.c
+++ handle_binary.c
@@ -8,27 +8,30 @@
  */
 int print_bin(va_list args, int count)
 {
-	unsigned int n, m, i, sum;
-	unsigned int a[32];
+int arr[32];
+int num, i = 0;
+char bit;
+num = va_arg(args, int);
+if (num == 0)
+{
+	write(1, "0", 1);
+	return (++count);
+}
 
-	n = va_arg(args, unsigned int);
-	m = 2147483648; /* (2 ^ 31) */
-	a[0] = n / m;
-	for (i = 1; i < 32; i++)
-	{
-		m /= 2;
-		a[i] = (n / m) % 2;
-	}
-	for (i = 0, sum = 0, count = 0; i < 32; i++)
-	{
-		sum += a[i];
-		if (sum || i == 31)
-		{
-			char z = '0' + a[i];
+while (num != 0)
+{
+	arr[i] = num % 2;
+	num /= 2;
+	i++;
+}
 
-			write(1, &z, 1);
-			count++;
-		}
-	}
-	return (count);
+count += i;
+
+for (i = i - 1; i >= 0; i--)
+{
+	bit = arr[i] + '0';
+	write(1, &bit, 1);
+}
+
+return (count);
 }
