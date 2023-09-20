3101e1cc72dc3c05c750c902f2af7f7f450ce52d
DuaaRabie <Duaarabie11@gmail.com>
2023-09-19 19:22:34 -0700

    update handle pointers

--- handle_pointer.c
+++ handle_pointer.c
@@ -11,30 +11,13 @@
 int handle_pointer(va_list args, int count)
 {
 	void *ptr;
-	int i, h;
-	char arr[16];
 	long int numptr;
 
 	ptr = va_arg(args, void*);
-
 	numptr = (long int)ptr;
-	i = 0;
-	while (numptr != 0)
-	{
-		h = (numptr % 16);
-		if (h < 10)
-			h += '0';
-		else
-			h += 'a' - 10;
-		arr[i] = h;
 
-		numptr /= 16;
-		i++;
-	}
-	i--;
 	count += write(1, "0x", 2);
-	while (i >= 0)
-		count += write(1, &arr[i--], 1);
-
+	count += hex_digit_print(numptr);
+	
 	return (count);
 }
